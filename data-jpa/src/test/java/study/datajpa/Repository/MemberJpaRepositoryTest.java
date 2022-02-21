79efcc59adf425ec8f7217dcaa6887f791403675
emmababy <bomvll@naver.com>
2022-02-21 21:55:29 +0900

    Method name Query

--- data-jpa/src/test/java/study/datajpa/Repository/MemberJpaRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/Repository/MemberJpaRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberJpaRepositoryTest {
 
     @Autowired MemberJpaRepository memberJpaRepository;
+    @Autowired MemberRepository memberRepository;
 
     @Test
     public void testMember() {
@@ -34,5 +37,55 @@ class MemberJpaRepositoryTest {
 
     }
 
+    @Test
+    public void basicCRUD() {
+        //given
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        //when
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        //then
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        findMember1.setUsername("member!!!!!!");
+
+
+
+//        //리스트조회검증
+//        List<Member> all = memberJpaRepository.findAll();
+//        assertThat(all.size()).isEqualTo(2);
+//
+//        long count = memberJpaRepository.count();
+//        assertThat(count).isEqualTo(2);
+//
+//        //삭제검증
+//        memberJpaRepository.delete(member1);
+//        memberJpaRepository.delete(member2);
+//
+//        long deletedcount = memberJpaRepository.count();
+//        assertThat(deletedcount).isEqualTo(0);
+    }
+
+
+    @Test
+    public void findByUsernameAndAgeGreaterThan() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+    }
 
 }
--- data-jpa/src/test/java/study/datajpa/Repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/Repository/MemberRepositoryTest.java
@@ -30,4 +30,40 @@ class MemberRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
     }
+
+    @Test
+    public void basicCRUD() {
+        //given
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        //then
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        findMember1.setUsername("member!!!!!!");
+
+
+
+//        //리스트조회검증
+//        List<Member> all = memberJpaRepository.findAll();
+//        assertThat(all.size()).isEqualTo(2);
+//
+//        long count = memberJpaRepository.count();
+//        assertThat(count).isEqualTo(2);
+//
+//        //삭제검증
+//        memberJpaRepository.delete(member1);
+//        memberJpaRepository.delete(member2);
+//
+//        long deletedcount = memberJpaRepository.count();
+//        assertThat(deletedcount).isEqualTo(0);
+    }
+
 }
