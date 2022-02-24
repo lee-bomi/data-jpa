a3b0e6138c289a54974285fc9ab8db93ee78a092
Emma <emma@rtruesoft.kr>
2022-02-24 17:42:04 +0900

    Pure JPA paging and align

--- data-jpa/src/test/java/study/datajpa/Repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/Repository/MemberRepositoryTest.java
@@ -100,4 +100,17 @@ class MemberRepositoryTest {
         }
     }
 
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA");
+        Member m2 = new Member("BBB");
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> aaa = memberRepository.findListByUsername("AAA");
+        for (Member member : aaa) {
+            System.out.println("member = " + member);
+        }
+    }
+
 }
